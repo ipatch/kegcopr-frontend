6f2dfbcd00a59d8e67e6bc2fa3e0ddabe501d1a4
ipatch <chris.r.jones.1983@gmail.com>
2017-05-05 13:03:26 -0500

    create Login & Signup containers, and relocate Home container

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 import { BrowserRouter, Match, Miss } from 'react-router';
 // changed the below line from '../Home' to './Home' in order to get app
 // to compile.
-import Home from './Home';
+import Home from '../Home';
 import NotFound from '../../components/NotFound';
+import Login from '../Login';
+import Signup from '../Signup';
 
 class App extends Component {
   render() {
@@ -12,6 +14,8 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Match exactly pattern="/" component={Home} />
+          <Match pattern="/login" component={Login} />
+          <Match pattern="/signup" component={Signup} />
           <Miss component={NotFound} />
         </div>
       </BrowserRouter>
