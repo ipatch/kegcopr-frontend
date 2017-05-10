555be4702d08496cdf2c7f3cead8ef08b928bf3c
ipatch <chris.r.jones.1983@gmail.com>
2017-05-09 22:22:17 -0500

    tshooting frontend

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,70 +1,110 @@
 // @flow
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { Link } from 'react-router-dom';
-import { css, StyleSheet } from 'aphrodite';
-import Input from '../../components/Input';
+// need to figure out how to import PropTypes
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { login } from '../../actions/session';
+import LoginForm from '../../components/LoginForm';
+import Navbar from '../../components/Navbar';
+// import { Field, reduxForm } from 'redux-form';
+// import { Link } from 'react-router-dom';
+// import { css, StyleSheet } from 'aphrodite';
+// import Input from '../../components/Input';
 
-const styles = StyleSheet.create({
-  card: {
-    maxWidth: '500px',
-    padding: '3rem 4rem',
-    margin: '2rem auto',
-  },
-});
+// const styles = StyleSheet.create({
+//   card: {
+//     maxWidth: '500px',
+//     padding: '3rem 4rem',
+//     margin: '2rem auto',
+//   },
+// });
+
+// type Props = {
+//   onSubmit: () => void,
+//   handleSubmit: () => void,
+//   submitting: boolean,
+// }
 
 type Props = {
-  onSubmit: () => void,
-  handleSubmit: () => void,
-  submitting: boolean,
+  login: () => void,
 }
 
-class LoginForm extends Component {
+class Login extends Component {
+  static contextTypes = {
+    router: PropTypes.object,
+  }
+
   props: Props
 
-  handleSubmit = (data) => this.props.onSubmitWithProps(data);
-  // handleSubmit: (data) => this.props.onSubmit(data);
-
+  handleLogin = (data) => this.props.login(data, this.context.router);
 
   render() {
-    const { handleSubmit, submitting } = this.props;
-
     return (
-      <form
-        className={`card ${css(styles.card)}`}
-        onSubmit={handleSubmit(this.handleSubmit)}
-      >
-        <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Login to Sling</h3>
-        <Field name="email" type="text" component={Input} placeholder="Email" />
-        <Field name="password" type="password" component={Input} placeholder="Password" />
-        <button
-          type="submit"
-          disabled={submitting}
-          className="btn btn-block btn-primary"
-        >
-          {submitting ? 'Logging in...' : 'Login'}
-        </button>
-        <hr style={{ margin: '2rem 0' }} />
-        <Link to="/signup" className="btn btn-block btn-secondary">
-          Create a new account
-        </Link>
-      </form>
+      <div style={{ flex: '1' }}>
+        <Navbar />
+        <LoginForm onSubmit={this.handleLogin} />
+      </div>
     );
   }
 }
 
-const validate = (values) => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = 'Required';
-  }
-  if (!values.password) {
-    errors.password = 'Required';
-  }
-  return errors;
-};
+export default connect(null, { login })(Login);
 
-export default reduxForm({
-  form: 'login',
-  validate,
-})(LoginForm);
+// class LoginForm extends Component {
+  // props: Props
+
+  // This function is the one you're passing into the form, and it'll be called
+  // to handle form submission.
+  // Do anything you want here, I think
+
+  // handleSubmit = (data) => this.props.onSubmit(data);
+
+  // handleSubmit: (data) => this.props.onSubmit(data);
+
+//   handleSubmit = (data) => { console.log('submitted!') }
+//
+//
+//   render() {
+//     // Correct here, handleSubmit is injected by the call to reduxForm()
+//     // at the bottom.
+//     const { handleSubmit, submitting } = this.props;
+//
+//     return (
+//       <form
+//         className={`card ${css(styles.card)}`}
+//         onSubmit={handleSubmit(this.handleSubmit)}
+//       >
+//         <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Login to Keg Cop R</h3>
+//         <Field name="email" type="text" component={Input} placeholder="Email" />
+//         <Field name="password" type="password" component={Input} placeholder="Password" />
+//         <button
+//           type="submit"
+//           disabled={submitting}
+//           className="btn btn-block btn-primary"
+//         >
+//           {submitting ? 'Logging in...' : 'Login'}
+//         </button>
+//         <hr style={{ margin: '2rem 0' }} />
+//         <Link to="/signup" className="btn btn-block btn-secondary">
+//           Create a new account
+//         </Link>
+//       </form>
+//     );
+//   }
+// }
+//
+// const validate = (values) => {
+//   const errors = {};
+//   if (!values.email) {
+//     errors.email = 'Required';
+//   }
+//   if (!values.password) {
+//     errors.password = 'Required';
+//   }
+//   return errors;
+// };
+//
+// export default reduxForm({
+//   form: 'login',
+//   validate,
+// })(LoginForm);
