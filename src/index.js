08299fe46d3d1248335be2f2dedfcb0794913c41
Chris Jones <chris.r.jones.1983@gmail.com>
2019-01-04 15:20:08 -0600

    wow so many changes, migrate to mulit webpack config update all top level pkgs in `package.json` add simple little hello.js react component for sanity and app should be running in dev mode now

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './containers/App';
+// import { Provider } from 'react-redux';
+// import store from './store';
+// import App from './containers/App';
 // import App from './App';
 // import './index.css';
+import Hello from './hello';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+  <Hello />,
+  document.getElementById('app')
+)
+
+// ReactDOM.render(
+//   // <Provider store={store}>
+//     // <App />
+//   // </Provider>,
+//   document.getElementById('root')
+// );
