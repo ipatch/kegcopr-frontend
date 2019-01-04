12b22970379d9925356425bd87bf97a75d53bf93
Chris Jones <chris.r.jones.1983@gmail.com>
2019-01-04 15:21:30 -0600

    wow so many changes, migrate to mulit webpack config update all top level pkgs in `package.json` add simple little hello.js react component for sanity and app should be running in dev mode now

---
+++ src/hello.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Hello extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Hello from JSX!</h1>
+      </div>
+    )
+  }
+}
+
+export default Hello
